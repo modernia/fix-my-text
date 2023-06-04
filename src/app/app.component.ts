a6d9c32d7dbeefeb343fc7f79069a06f281aa0d6
Miguel Martinez <miguel.emezero@gmail.com>
2023-06-03 22:14:59 -0600

    👾 feat: main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from 'openai';
+import { environment } from 'src/environments/environment';
+import { FixService } from './services/fix.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translate-ai';
+  phrase!: string;
+  fixedPhrase!:string | undefined;
+  isTooShort:boolean = false;
+  loading:boolean  = false;
+  fixService: FixService = inject(FixService);
+
+  dummy:string = "asdsadasasddsasaddsa asdasdds23e1 kjh";
+
+  
+
+  onPhraseChange(e: any) {
+    this.phrase = e.target.value;
+  }
+
+  async onSubmit() {
+    if(this.phrase === undefined || this.phrase.length === 0) {
+      return
+    }
+    if(this.phrase.length < 10) {
+      this.isTooShort = true;
+      return
+    }
+    this.loading = true;
+    this.fixedPhrase = await this.fixService.fixMyPhrase(this.phrase) as string;
+    console.log(this.fixedPhrase)
+    this.loading = false;
+  }
 }
